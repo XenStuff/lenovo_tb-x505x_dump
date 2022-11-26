542bac968391cf093561c1b75e7c400fbbcaacd1
xenxynon <flynryder427@gmail.com>
2022-11-26 02:02:27 +0000

    Add extras for msm8937_64-user 10 QKQ1.191224.003 X505X_S001148_220902_ROW release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## msm8937_64-user 10 QKQ1.191224.003 X505X_S001148_220902_ROW release-keys
+- Manufacturer: lenovo
+- Platform: msm8937
+- Codename: TB-X505X
+- Brand: Lenovo
+- Flavor: msm8937_64-user
+- Release Version: 10
+- Id: QKQ1.191224.003
+- Incremental: X505X_S001148_220902_ROW
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: Lenovo/TB-X505X/TB-X505X:10/QKQ1.191224.003/X505X_S001148_220902_ROW:user/release-keys
+- OTA version: 
+- Branch: msm8937_64-user-10-QKQ1.191224.003-X505X_S001148_220902_ROW-release-keys
+- Repo: lenovo_tb-x505x_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/app/YandexBrowser/YandexBrowser.apk.* 2>/dev/null >> system/system/app/YandexBrowser/YandexBrowser.apk
+rm -f system/system/app/YandexBrowser/YandexBrowser.apk.* 2>/dev/null
+cat system/system/app/Alexa/Alexa.apk.* 2>/dev/null >> system/system/app/Alexa/Alexa.apk
+rm -f system/system/app/Alexa/Alexa.apk.* 2>/dev/null
+cat system/system/app/MonsterClass/MonsterClass.apk.* 2>/dev/null >> system/system/app/MonsterClass/MonsterClass.apk
+rm -f system/system/app/MonsterClass/MonsterClass.apk.* 2>/dev/null
+cat system/system/app/YandexSearch/YandexSearch.apk.* 2>/dev/null >> system/system/app/YandexSearch/YandexSearch.apk
+rm -f system/system/app/YandexSearch/YandexSearch.apk.* 2>/dev/null
+cat system/system/app/ParentingBook/ParentingBook.apk.* 2>/dev/null >> system/system/app/ParentingBook/ParentingBook.apk
+rm -f system/system/app/ParentingBook/ParentingBook.apk.* 2>/dev/null
+cat system/system/app/Outlook/Outlook.apk.* 2>/dev/null >> system/system/app/Outlook/Outlook.apk
+rm -f system/system/app/Outlook/Outlook.apk.* 2>/dev/null
+cat system/system/priv-app/Vega_FactoryGM/Vega_FactoryGM.apk.* 2>/dev/null >> system/system/priv-app/Vega_FactoryGM/Vega_FactoryGM.apk
+rm -f system/system/priv-app/Vega_FactoryGM/Vega_FactoryGM.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/priv-app/Settings2/Settings2.apk.* 2>/dev/null >> system/system/product/priv-app/Settings2/Settings2.apk
+rm -f system/system/product/priv-app/Settings2/Settings2.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
